8363404311fad0aed25a4014c3900c3b7155d77f
Carlos Levir <carloslevirfs@gmail.com>
2019-05-23 15:29:28 -0300

    Project Refactoring.

--- src/server.js
+++ src/server.js
@@ -1,37 +1,48 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const ejs = require('ejs');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const ejs = require("ejs");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
-app.set('views', path.resolve(__dirname, '..', 'public'));
+app.use(express.static(path.resolve(__dirname, "..", "public")));
+app.set("views", path.resolve(__dirname, "..", "public"));
 
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
 
-app.use('/', (req, res) => {
-  res.render('index.html');
+app.use("/", (req, res) => {
+  res.render("index.html");
 });
 
 let messages = [];
+let socketIds = [];
 
-io.on('connection', socket => {
-  socket.emit('previousMessages', messages);
-  socket.on('newMessage', newMessage => {
+io.on("connection", socket => {
+  socketIds[socket.request.connection._peername.address.replace(/[f:]/g, "")] =
+    socket.id;
+  socket.emit("previousMessages", messages);
+  socket.on("newMessage", data => {
+    const { newMessage, ip } = data;
     const { address, port } = socket.request.connection._peername;
 
-    messages.push({ address: `${address}:${port}`, newMessage });
+    if (ip !== "") {
+      io.to(socketIds[ip]).emit("receivedMessage", {
+        address: `${address}:${port}`,
+        newMessage
+      });
+    } else {
+      messages.push({ address: `${address}:${port}`, newMessage });
 
-    io.emit('receivedMessage', { address: `${address}:${port}`, newMessage });
-  })
+      io.emit("receivedMessage", { address: `${address}:${port}`, newMessage });
+    }
+  });
 });
 
 server.listen(process.env.PORT || 3000);
